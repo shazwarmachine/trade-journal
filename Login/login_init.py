506b6f5ac709a85e2c25f43b9d4a05f2a4b236ab
akshayhhh522 <akshayhhh522@gmail.com>
2025-07-05 01:53:32 +0530

    req

--- Login/login_init.py
+++ Login/login_init.py
@@ -11,14 +11,13 @@ kite = KiteConnect(api_key="35msa178vtcy64ws")
 # from the registered redirect url after the login flow.
 # Once you have the request_token, obtain the access_token
 # as follows.
-
 print(kite.login_url())
 # data = kite.generate_session("USY5sK47LqE5b8zylt4FDQ2XY1aTY2Rv", api_secret="o816jxsbhtrcz0z163242ahvgvtd6w6w")
 # print(data)
-kite.set_access_token("USY5sK47LqE5b8zylt4FDQ2XY1aTY2Rv")
-kite.holdings()
-kite.trades()
-kite.profile()
+# kite.set_access_token("USY5sK47LqE5b8zylt4FDQ2XY1aTY2Rv")
+# kite.holdings()
+# kite.trades()
+# kite.profile()
 
 def generate_session_using_request_token(request_token):
     """
@@ -28,6 +27,7 @@ def generate_session_using_request_token(request_token):
 
     data = kite.generate_session(request_token, api_secret="o816jxsbhtrcz0z163242ahvgvtd6w6w")
     kite.set_access_token(data["access_token"])
+    print(data["access_token"])
 
 
 def login_init() :
